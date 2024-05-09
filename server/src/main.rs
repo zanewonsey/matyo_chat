b58e3a6749d43932c4c4b58d8c5b533f93a3b68b
Zane Wonsey <zanewonsey@gmail.com>
2024-05-09 06:57:28 -0400

    Adding in basic conncetion_listener
    Currently has a hard coded address to bind to

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,27 @@
+use std::net::TcpListener;
+
+fn conncetion_listener() {
+
+    let addr = "127.0.0.1:80";
+    let listener = match TcpListener::bind(addr) {
+        Ok(bound_listener) => {
+            println!("Listener bound to addr {}", addr);
+            bound_listener
+        },
+        Err(_) => todo!(),
+    };
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("A client onnected");
+            },
+            Err(_) => todo!(),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    conncetion_listener()
 }
